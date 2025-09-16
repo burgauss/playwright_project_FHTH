57ccb1d9f18775d7ff6a1a49db0a19d2e47410af
Burgauss <burgos.135@gmail.com>
2025-09-15 18:51:12 -0600

    implemented locator reuse

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,4 +1,4 @@
-import {test} from '@playwright/test'
+import {test, expect} from '@playwright/test'
 
 test.beforeEach(async ({page}) => {
     await page.goto('http://localhost:4200/')
@@ -82,4 +82,16 @@ test('Locating parent elements', async ({page}) => {
     await page.locator(':text-is("Using the Grid")').locator('..').getByRole('textbox', {name: "Email"}).click()
 })
 
-text('Reusin')
+test('Reusing locators', async ({page}) => {
+    const basicForm = page.locator('nb-card').filter({hasText:"Basic form"})
+    const emailField = basicForm.getByRole('textbox', {name: 'Email'})
+
+    await emailField.fill('test@test.com')
+    await basicForm.getByRole('textbox', {name: 'Password'}).fill('Welcome123')
+    await basicForm.locator('nb-checkbox').click()
+    await basicForm.getByRole('button').click()
+
+    await expect(emailField).toHaveValue('test@test.com')
+
+
+})
